71508884938b89f3e776757625031d789236fa4c
Dopa-Mina <y.kuhl.mihopulos@gmail.com>
2018-05-05 17:40:39 +0200

    rename

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var name = "Yasemina"
+	var alter = 29
+
+	fmt.Printf("Mein Name ist %s ", name)
+	fmt.Printf("und ich bin %v Jahre alt", alter)
+
+}
