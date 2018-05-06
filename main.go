aab2cfcf6ee815cae64be05343d9f63a1a73ed33
Dopa-Mina <y.kuhl.mihopulos@gmail.com>
2018-05-06 17:28:01 +0200

    Revert "git practice"
    This reverts commit 9ae07673286304a692b5bdda6e1f1cfa9ff2afa9.

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 )
 
 func main() {
@@ -10,12 +9,7 @@ func main() {
 	var name = "Yasemina"
 	var alter = 29
 
-	if alter == 29 {
-		//log.Fatal("you shall not pass")
-		log.Printf("user ist ganz schön alt nämlich: %d \n", alter)
-	}
-
-	fmt.Printf("Mein Name ist %s \n", name)
-	fmt.Printf("und ich bin %v Jahre alt \n", alter)
+	fmt.Printf("Mein Name ist %s ", name)
+	fmt.Printf("und ich bin %v Jahre alt", alter)
 
 }
