5e17ccb37a494f45657a763421c203628cc1daa0
Dopa-Mina <y.kuhl.mihopulos@gmail.com>
2018-05-23 17:20:15 +0200

    Fizz-Buzz

--- loops/main.go
+++ loops/main.go
@@ -5,15 +5,35 @@ import (
 )
 
 func main() {
-	a := 1
 
-	for a <= 10 {
-		fmt.Println(a)
-		a++
-	}
+	for i := 1; i <= 100; i++ {
+		PrintNr := true
 
-	for i := 1; i <= 10; i++ {
-		fmt.Println(i)
+		if i%3 == 0 {
+			fmt.Println("Fizz")
+			PrintNr = false
+		}
+		if i%5 == 0 {
+			fmt.Println("Buzz")
+			PrintNr = false
+		}
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+			PrintNr = false
+		}
+
+		if PrintNr {
+			fmt.Println(i)
+		}
 	}
 
 }
+
+/*
+	Erstelle ein Programm, welches die Zahlen von 1 bis 100
+	ausgibt. Wenn die Zahl jedoch durch 3 teilbar ist, dann
+	gib Fizz aus. Wenn die Zahl durch 5 teilbar ist, dann gib
+	Buzz aus. Für Zahlen, welche durch 3 und durch 5 teilbar
+	sind (z.B. 15) gib FizzBuzz aus.
+
+*/
