4bdf50dcd4a0956a1cbd41cd4ad923cce43d1bc4
Dopa-Mina <y.kuhl.mihopulos@gmail.com>
2018-05-24 13:26:02 +0200

    new-map-with-loop

--- maps/main.go
+++ maps/main.go
@@ -4,24 +4,21 @@ import (
 	"fmt"
 )
 
-type person struct {
-	name     string
-	nachname string
-}
-
 func main() {
 
-	p1 := person{"Max", "Mustermann"}
-	p2 := person{"Moritz", "Mustermann"}
-	p3 := person{"Michelle", "Mustermann"}
-	p4 := person{"Manuel", "Mustermann"}
-
-	isAdmin := make(map[person]bool)
-	isAdmin[p1] = true
-	isAdmin[p2] = false
-	isAdmin[p3] = false
-	isAdmin[p4] = false
-
-	fmt.Println(p1, isAdmin[p1])
-
+	digit := map[int]string{
+		1:  "one",
+		2:  "two",
+		3:  "three",
+		4:  "four",
+		5:  "five",
+		6:  "six",
+		7:  "seven",
+		8:  "eight",
+		9:  "nine",
+		10: "ten",
+	}
+	for i := 1; i <= len(digit); i++ {
+		fmt.Println(digit[i])
+	}
 }
