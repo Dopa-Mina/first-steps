db3b930ef26a2380d8cef847436d83a5ba770001
Dopa-Mina <y.kuhl.mihopulos@gmail.com>
2018-06-10 15:41:49 +0200

    func practice

--- function/first-try/main.go
+++ function/first-try/main.go
@@ -7,10 +7,33 @@ import (
 func sub(a int, b int) int {
 	c := a - b
 	return c
+
+}
+func add(d int, f int) int {
+	g := d + f
+	return g
+
+}
+func div(h int, i int) int {
+	j := h / i
+	return j
+
+}
+func mul(k int, l int) int {
+	j := k * l
+	return j
+
 }
 
 func main() {
-	difference1 := sub(10, 5)
-	difference2 := sub(27, 2)
-	fmt.Println(difference1, difference2)
+	diff1 := sub(10, 5)
+	diff2 := sub(27, 2)
+	diff3 := add(28, 4)
+	diff4 := add(29, 3)
+	diff5 := div(300, 15)
+	diff6 := div(210, 10)
+	diff7 := mul(23, 13)
+	diff8 := mul(7, 8)
+
+	fmt.Println(diff1, diff2, diff3, diff4, diff5, diff6, diff7, diff8)
 }
